1acb44494b827a03f7fe396c4d5cff70300fd302
NAOMIE-PC\Naomie Sonny-Cole <naomie.sonny-cole@sirom.fr>
2016-12-28 09:19:50 +0100

    Affichage des différents articles

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -8,32 +8,54 @@ use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends Controller
 {
-    /**
-     * @Route("/blog/blog", name="blog")
-     */
-    public function blogAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('blog/blog.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
-    }
+  /**
+  * @Route("/blog/blog", name="blog")
+  */
+  public function blogAction(Request $request)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $articles = $em->getRepository('AppBundle:Article')->getAllArtcicles();
+    return $this->render('blog/blog.html.twig', array(
+      'articles' => $articles
+    ));
 
-    /**
-     * @Route("/blog/afficheParCat", name="afficheParCat")
-     */
-    public function afficheParCatAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('blog/afficheParCat.html.twig');
-    }
+  }
+  /**
+  * @Route("/blog/categorie/{id}", name="afficheParCat", requirements={"id" = "\d+"})
+  */
+  public function afficheParCatAction(Request $request,$id)
+  {
+    $categorie = $this->getDoctrine()->getRepository('AppBundle:Categorie')->getById($id);
+    $articleCategorie = $this->getDoctrine()->getRepository('AppBundle:Article')->findBy(array('categorie' => $categorie));
+    return $this->render('blog/afficheParCat.html.twig', array(
+      'articles' => $articleCategorie,
+    ));
+  }
 
-    /**
-     * @Route("/blog/afficheArticle", name="afficheArticle")
-     */
-    public function afficheArticleAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('blog/afficheArticle.html.twig');
-    }
+
+  /**
+  * @Route("/blog/afficheArticle/{id}", name="afficheArticle", requirements={"id" = "\d+"})
+  */
+  public function afficheArticleAction(Request $request,$id)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $article = $em->getRepository('AppBundle:Article')->getById($id);
+
+    return $this->render('blog/afficheArticle.html.twig', array(
+      'Article' => $article,
+
+    ));
+  }
+
+  /**
+  * @Route("/blog/afficheParCat/{id}", name="afficheParCatArticle, requirements={"id" = "\d+"}")
+  */
+  /*public function afficheParCatArticleAction(Request $request,$id)
+  {
+    $categorie = $this->getDoctrine()->getRepository('AppBundle:Categorie')->getArticles($id);
+    return $this->render('blog/afficheParCatArticle.html.twig',array(
+      'categorie' => $categorie,
+    ));
+  }*/
+
 }
